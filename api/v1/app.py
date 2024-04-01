0f08a47318bac9f9f241c6a9fabeebb91d9526d9
Cheikyadam <urbaingande@gmail.com>
2024-04-01 16:03:58 -0700

    404

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-
+from flask import make_response, jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,6 +14,11 @@ app.register_blueprint(app_views)
 def teardown_app_context(exception=None):
     """tear down"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
