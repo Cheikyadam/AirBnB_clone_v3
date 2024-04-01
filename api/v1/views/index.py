6107851bf4f60f9e9a02c288bdcc8b4e147d97e8
Cheikyadam <urbaingande@gmail.com>
2024-04-01 16:58:20 -0700

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,23 @@
 """Index page"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def func():
     """Return status"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats')
+def stat():
+    """Return stats"""
+    modstats = {}
+    modstats['amenities'] = storage.count("Amenity")
+    modstats['cities'] = storage.count("City")
+    modstats['places'] = storage.count("Place")
+    modstats['reviews'] = storage.count("Review")
+    modstats['states'] = storage.count("State")
+    modstats['users'] = storage.count("User")
+    return jsonify(modstats)
