e69d247f66509e44d78719836e9beef687c422ce
Cheikyadam <urbaingande@gmail.com>
2024-04-02 13:16:00 -0700

    db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -53,11 +53,20 @@ class DBStorage:
 
     def get(self, cls, id):
         """method to retrieve one object"""
-        if cls in classes.values() or cls in classes:
-            if cls in classes:
-                cls = classes[cls]
-            obj = self.__session.query(cls).filter(cls.id == id)
-            return obj.first()
+        #if cls in classes.values() or cls in classes:
+            #if cls in classes:
+               #cls = classes[cls]
+            #obj = self.__session.query(cls).filter(cls.id == id)
+            #return obj.first()
+        allobj = self.all()
+        if cls in classes:
+            cls = classes[cls]
+        if allobj is not None:
+            idr = cls.__name__ + "." + id
+            try:
+                return allobj[idr]
+            except KeyError:
+                return None
 
     def count(self, cls=None):
         """To count number of obj """
